03c26a2d5303681ce8f783c4899f3065e34d9970
Santiago Cordero <santicordero28@gmail.com>
2025-09-01 22:11:25 -0600

    Terminado el inciso #2.

--- src/p2_ring.cpp
+++ src/p2_ring.cpp
@@ -21,42 +21,67 @@ void ring_push(Ring* r, int v){
     
     while(r->count == Q && !r->stop){
         pthread_cond_wait(&r->not_full,&r->m);
-        if(!r->stop){ 
-            r->buf[r->head]=v; 
-            r->head=(r->head+1) % Q; 
-            r->count++; 
-            pthread_cond_signal(&r->not_empty); 
-        }
-        pthread_mutex_unlock(&r->m);
     }
+    if(!r->stop){ 
+        r->buf[r->head]=v; 
+        r->head=(r->head+1) % Q; 
+        r->count++; 
+        pthread_cond_signal(&r->not_empty); 
+    }
+    pthread_mutex_unlock(&r->m);
 }
+
 
 bool ring_pop(Ring* r, int* out){
     pthread_mutex_lock(&r->m); 
     while(r->count==0 && !r->stop){
-        pthread_cond_wait(&r->not_empty,&r->m); 
-        if(r->count==0 && r->stop){ 
-            pthread_mutex_unlock(&r->m); 
-            return false; 
-        } 
-        *out=r->buf[r->tail]; 
-        r->tail=(r->tail+1)%Q; 
-        r->count--;
+        pthread_cond_wait(&r->not_empty,&r->m);
+    }
+    if(r->count==0 && r->stop){ 
+        pthread_mutex_unlock(&r->m); 
+        return false; 
+    } 
+    *out=r->buf[r->tail]; 
+    r->tail=(r->tail+1)%Q; 
+    r->count--;
     pthread_cond_signal(&r->not_full); 
     pthread_mutex_unlock(&r->m);
     return true;
+}
+
+void* producerThread(void* arg){
+    Ring* r = static_cast<Ring*>(arg);
+    for (int i = 0; i < 25; i++){
+        ring_push(r, i);
+        printf("Pusheado %d al ring\n",i);
     }
+    pthread_mutex_lock(&r->m);
+    r->stop = true;
+    pthread_cond_broadcast(&r->not_empty);
+    pthread_mutex_unlock(&r->m);
+    
+    return nullptr;
+}
+
+void* consumerThread(void* arg){
+    Ring* r = static_cast<Ring*>(arg);
+    int val;
+    while (ring_pop(r, &val)){
+        printf("Consumidor sacó %d del ring\n", val);
+    }
+    return nullptr;
 }
 
 int main(int argc, char** argv){
     Ring* r = new Ring;
-    // primer parámetro de la ejecución dicta cuántos hilos tratarán de sacar de la cola
-    int producer = (argc > 1) ? atoi(argv[1]) : 1;
 
-    pthread_t ids[producer];
-    
-    for (int i = 0; i < producer; i++){
-        pthread_create(&ids[i], nullptr, ring_push, r);
-    }
+    pthread_t producer, consumer;
+    pthread_create(&producer, nullptr, producerThread, r);
+    pthread_create(&consumer, nullptr, consumerThread, r);
 
+    pthread_join(producer, nullptr);
+    pthread_join(consumer, nullptr);
+
+    delete r;
+    return 0;
 }
