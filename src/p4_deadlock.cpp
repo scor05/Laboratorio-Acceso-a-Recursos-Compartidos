2e7b1848a76f2e6ded5aea295acb376d10c0d5b3
Santiago Cordero <santicordero28@gmail.com>
2025-09-02 18:09:21 -0600

    Corregido el deadlock de la práctica #4 y terminado la práctica #5

--- src/p4_deadlock.cpp
+++ src/p4_deadlock.cpp
@@ -3,20 +3,27 @@
 #include <cstdio> 
 
 pthread_mutex_t A=PTHREAD_MUTEX_INITIALIZER, B=PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond;
 
+// Solución con orden global para que todos los hijos ejecuten el código
+// en el mismo orden para no causar el deadlock de A y B
 void* t1(void*){ 
-    pthread_mutex_lock(&A); 
+    pthread_mutex_lock(&A);
     usleep(1000);
-    pthread_mutex_lock(&B); 
-    std::puts("t1 ok"); 
+    pthread_mutex_lock(&B);
+    std::puts("t1 ok");
+    pthread_mutex_unlock(&B);
+    pthread_mutex_unlock(&A);
     return nullptr;
 }
 
 void* t2(void*){ 
-    pthread_mutex_lock(&B); 
-    usleep(1000);
-    pthread_mutex_lock(&A); 
-    std::puts("t2 ok"); 
+    pthread_mutex_lock(&A);
+    usleep(1000); 
+    pthread_mutex_lock(&B);
+    std::puts("t2 ok");
+    pthread_mutex_unlock(&B);
+    pthread_mutex_unlock(&A);
     return nullptr; 
 }
 
---
+++ src/p5_pipeline.cpp
@@ -0,0 +1,94 @@
+#include <pthread.h>
+#include <cstdio>
+#include <cstdlib>
+
+constexpr int TICKS = 1000;
+static pthread_barrier_t barrier1, barrier2, barrier3, barrier4;
+static pthread_once_t once_flag = PTHREAD_ONCE_INIT;
+
+struct Args{
+    int* dato;
+    bool* isEven;
+    long id;
+    int* acumulado;
+};
+
+FILE* logfile = nullptr;
+
+static void init_shared(){
+    /* abrir archivo de log, reservar buffers, etc.*/
+    logfile = fopen("data/pipeline_log.txt", "w");
+    if (!logfile) {
+        perror("Error abriendo log");
+        exit(1);
+    }
+    fprintf(logfile, "Pipeline inicializado\n");
+} 
+
+void* stage(void* p){
+    Args* arg = static_cast<Args*>(p);
+    long id = arg->id;
+    bool* isEven = arg->isEven;
+    int* num = arg->dato;
+    int* acumulado = arg->acumulado;
+
+    pthread_once(&once_flag, init_shared); 
+    for(int t=0;t<TICKS;t++){
+        /* trabajo */
+        /* 
+            stage 1 -> Generar aleatorio
+            stage 2 -> filtrar impares (que solo sigan al 3 los pares)
+            stage 3 -> sumar todos los números filtrados (reducir)
+        */
+        pthread_barrier_wait(&barrier1);
+        if (id == 1){
+            *num = rand() % 100 + 1;
+            fprintf(logfile, "Tick #%d generó %d\n", t, *num);
+        }
+        pthread_barrier_wait(&barrier2);
+        if (id == 2){
+            if (*num % 2 == 1){
+                *isEven = false;
+                fprintf(logfile, "Tick #%d filtró %d\n", t, *num);
+            } else {
+                *isEven = true;
+                fprintf(logfile, "Tick #%d dejó pasar a %d\n", t, *num);
+            }
+        }
+        pthread_barrier_wait(&barrier3);
+        if (id == 3){
+            if (*isEven){
+                *acumulado += *num;
+                fprintf(logfile, "Tick #%d sumó %d al acumulado: %d\n", t, *num, *acumulado);
+            }
+        }
+        pthread_barrier_wait(&barrier4);
+    }
+
+    return nullptr;
+}
+
+int main(){
+    srand(time(NULL));
+    pthread_t h1,h2,h3;
+    int global = 0;
+    int num;
+    bool isEven = false;
+    Args* a1 = new Args{&num, &isEven, 1, &global};
+    Args* a2 = new Args{&num, &isEven, 2, &global};
+    Args* a3 = new Args{&num, &isEven, 3, &global};
+    pthread_barrier_init(&barrier1, NULL, 3);
+    pthread_barrier_init(&barrier2, NULL, 3);
+    pthread_barrier_init(&barrier3, NULL, 3);
+    pthread_barrier_init(&barrier4, NULL, 3);
+    pthread_create(&h1,0,stage,(void*)a1);
+    pthread_create(&h2,0,stage,(void*)a2);
+    pthread_create(&h3,0,stage,(void*)a3); 
+    pthread_join(h1,0);
+    pthread_join(h2,0); 
+    pthread_join(h3,0);
+    pthread_barrier_destroy(&barrier1);
+    pthread_barrier_destroy(&barrier2);
+    pthread_barrier_destroy(&barrier3);
+    pthread_barrier_destroy(&barrier4);
+} 
